e09f4f47c2b44ba70c4f9fb1c0597c0b218903f7
Nathan <nucktwillieren@gmail.com>
2021-07-13 07:01:14 +0800

    add arrow this

--- function/03.arrowThis.js
+++ function/03.arrowThis.js
@@ -4,12 +4,19 @@
 
 const myObject = {
     myMethod(items) {
-        console.log(this); // logs myObject
+        console.log("outside the callback", this); // logs myObject
         const callback = () => {
-            console.log(this); // logs myObject
+            console.log("in the callback:", this); // logs myObject
         };
         items.forEach(callback);
+    },
+    anotherMethod(items) {
+        console.log("outside the callback:", this); // logs myObject
+        items.forEach(() => {
+            console.log("in the callback:", this); // logs myObject
+        });
     }
 };
 
 myObject.myMethod([1, 2, 3]);
+myObject.anotherMethod([1, 2, 3])
